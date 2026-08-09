35dc90c977a74f98d011cfad4b893243787abe52
Ee Dhing <etan0105@student.monash.edu>
2026-08-10 00:35:30 +1000

    Added a lamp on the desk

---
+++ memory-museum/src/Lamp.jsx
@@ -0,0 +1,8 @@
+import { useGLTF } from '@react-three/drei'
+
+export default function Lamp({ position = [1.8, -0.82, -0.9], scale = 1 }) {
+  const { scene } = useGLTF('/models/lamp.glb')
+  return <primitive object={scene} position={position} scale={scale} />
+}
+
+useGLTF.preload('/models/lamp.glb')
