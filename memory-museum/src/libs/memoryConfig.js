341d9693c841f798e3acbccfdd6a8d4fafbb3ead
hooi0004 <hooi0004@student.monash.edu>
2026-08-11 14:31:25 +0800

    fix: fix location of items on desk

--- memory-museum/src/libs/memoryConfig.js
+++ memory-museum/src/libs/memoryConfig.js
@@ -1,28 +1,27 @@
-// add any new items here next time
 export const MEMORY_CONFIG = {
   book: {
-    position: [-1.5, -0.82, -0.4],
-    cameraTarget: [-1.5, -0.68, 0.25],
+    position: [-1.8, -0.82, -0.4],
+    cameraTarget: [-1.8, -0.68, 0.25],
     title: 'The Story We Wrote',
     color: '#8B0000',
     particleColor: '#e0a3a3',
   },
   letter: {
-    position: [-0.5, -0.82, 0.3],
-    cameraTarget: [0.7, -0.62, 0.75],
+    position: [-0.1, -0.82, 0.3],
+    cameraTarget: [-0.6, -0.62, 0.75],
     title: 'The Story We Read',
     color: '#f4ecd8',
     particleColor: '#f4ecd8',
   },
   record: {
-    position: [0.6, -0.82, -0.5],
-    cameraTarget: [-0.3, -0.55, -0.1],
+    position: [1.5, -0.7, 0.5],
+    cameraTarget: [0.6, -0.55, -0.1],
     title: 'The Story We Listen',
     color: '#c9a86a',
     particleColor: '#4a90d9',
   },
   picture: {
-    position: [-0.5, -0.3, -1.0],
+    position: [-0.6, -0.3, -1.1],
     cameraTarget: [1.9, -0.55, 0.0],
     title: 'The Story We Saw',
     color: '#2b2b2b',
