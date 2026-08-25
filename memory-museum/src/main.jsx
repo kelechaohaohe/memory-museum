cc3fba1aeec118fc9ed939de2283aa36020f7bb3
hooi0004 <hooi0004@student.monash.edu>
2026-08-25 16:18:31 +0800

    fix(router): add base router path

--- memory-museum/src/main.jsx
+++ memory-museum/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/memory-museum">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/memory/:memoryId" element={<App />} />
